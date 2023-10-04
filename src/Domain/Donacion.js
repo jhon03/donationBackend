834ea3fc1a9783f87bbeffba4394c060e21e1e29
jhon03 <hoyosb90@gmail.com>
2023-10-03 22:51:46 -0500

    Entidades donacion programa y proyecto

---
+++ src/Domain/Donacion.js
@@ -0,0 +1,18 @@
+class Donacion{
+
+
+    constructor(idDonacion, fechaCreacion, Estado, idProyecto, idBenefactor) {
+
+
+      this.idDonacion = idDonacion;
+      this.fechaCreacion = fechaCreacion;
+    
+      this.Estado = Estado;
+
+      this.Proyecto.idProyecto = idProyecto;
+      this.Donante.idBenefactor = idBenefactor;
+
+    }
+}
+
+module.exports = Donacion;
